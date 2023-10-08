7c547d39913dd30bbb4e3471e62331e7e3432b43
Yoohuan <141207744+Yoohuan@users.noreply.github.com>
2023-10-08 19:17:25 +0800

    Add files via upload

---
+++ Calculator/Calculator/Calculation.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+//加
+void MyPlus(char* p, int* n, int len, int i);
+
+//减
+void MySub(char* p, int* n, int len, int i);
+
+//乘
+void MyMul(char* p, int* n, int len, int i);
+
+//除
+int MyDiv(char* p, int* n, int len, int i);
+
+//括号
+int MyPare(char* p, int* n,int len, int i);
---
+++ Calculator/Calculator/test.c
@@ -0,0 +1,86 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Calculation.h"
+
+int main()
+{
+	printf("该程序仅能进行0-9的整数运算\n");
+	printf("仅可以使用单层 () 注意为英文括号\n");
+
+	while (1)
+	{
+		char input[1000] = { 0 };
+		int len = 0;
+		int num[1000] = { 0 };
+		printf("请输入计算式(输入 exit 退出程序):>");
+		scanf("%s", input);
+		if (strcmp(input, "exit") == 0)
+		{
+			break;
+		}
+		len = strlen(input);
+		int leg = 1;
+
+		for (int i = 0; i < len; i++)//识别数字并转化为整型
+		{
+			if (input[i] != '+' && input[i] != '-' && input[i] != '*' && input[i] != '/' && input[i] != '(' && input[i] != ')' && input[i] != '=')
+			{
+				num[i] = input[i] - '0';
+			}
+		}
+		for (int i = 0; i < len; i++)
+		{
+			if (input[i] == '(')
+			{
+				leg=MyPare(&input[i], &num[i],len, i);//括号内运算
+
+			}
+		}
+		if (leg == 0)
+		{
+			continue;
+		}
+		for (int i = 0; i < len; i++)
+		{
+			if (input[i] == '*')
+			{
+				MyMul(&input[i], &num[i], len, i);//执行乘法
+				i--;
+				len = len - 2;
+			}
+		}
+		for (int i = 0; i < len; i++)
+		{
+			if (input[i] == '/')
+			{
+				leg=MyDiv(&input[i], &num[i], len, i);//执行除法
+				i--;
+				len = len - 2;
+			}
+		}
+		if (leg == 0)
+		{
+			continue;
+		}
+		for (int i = 0; i < len; i++)
+		{
+			if (input[i] == '+')
+			{
+				MyPlus(&input[i],&num[i], len, i);//执行加法
+				i--;
+				len = len - 2;
+			}
+		}
+		for (int i = 0; i < len; i++)
+		{
+			if (input[i] == '-')
+			{
+				MySub(&input[i], &num[i], len, i);//执行减法
+				i--;
+				len = len - 2;
+			}
+		}
+		printf("%d\n\n", num[0]);
+	}
+	return 0;
+}
